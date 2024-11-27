05d6d143b98caaaaedbca0a07e7d3b595e75a8de
sehoon-jin <bang4won37@gmail.com>
2024-11-27 16:35:13 +0900

    Recruit 페이지 추가

--- src/pages/Recruit.tsx
+++ src/pages/Recruit.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import PageFrame from 'components/PageFrame';
+import styles from 'pages/Recruit.module.css';
+import CompanyContent from 'assets/images/contents/e68cc6d68b4719d5c359c7916712102a_1630862620_5204.jpg';
+import { useDefaultData } from 'commons/useDefaultData';
 
 export default function Recruit() {
+  const { menus } = useDefaultData();
+  const company = menus.filter((menu) => menu.id === 'rec');
+  const { id, to, name } = company[0];
   return (
-      <div>Recruit</div>
+    <PageFrame id={id} to={to} menu={name}>
+      <p className={styles.ctt_con_p}>
+        <img
+          src={CompanyContent}
+          title={`e68cc6d68b4719d5c359c7916712102a_1630862620_5204.jpg`}
+          alt={`Company`}
+        />
+        <br className={styles.ctt_con_p_br} />
+        &nbsp;
+      </p>
+    </PageFrame>
   );
 }
